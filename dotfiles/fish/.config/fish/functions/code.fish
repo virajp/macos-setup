dc62f9bb2a2a62a29131a4bffc437544113504f8
Viraj Patel <3125954+virajp@users.noreply.github.com>
2025-02-15 12:59:48 +0530

    update: add code & cursor functions

--- dotfiles/fish/.config/fish/functions/code.fish
+++ dotfiles/fish/.config/fish/functions/code.fish
@@ -7,9 +7,25 @@ function code
   if test -z "$ARGS"
     set ARGS "."
   end
-  $HOMEBREW_PREFIX/bin/cursor $ARGS
+  $HOMEBREW_PREFIX/bin/code $ARGS
 end
 
 function code95
   code ~/Projects/github.com/95octane/95octane.code-workspace
+end
+
+function cursor
+  set ARGS $argv
+  if test -z "$ARGS"
+    set ARGS (find . -maxdepth 1 -name "*.code-workspace")
+    set ARGS $ARGS:t
+  end
+  if test -z "$ARGS"
+    set ARGS "."
+  end
+  $HOMEBREW_PREFIX/bin/cursor $ARGS
+end
+
+function cursor95
+  cursor ~/Projects/github.com/95octane/95octane.code-workspace
 end
