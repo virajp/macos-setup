1f9896590f1cf78417a88009c3e535bf5f1426f6
Viraj Patel <3125954+virajp@users.noreply.github.com>
2025-10-09 19:04:57 +0530

    feat: add mise configuration file and update fish config for pnpm integration

--- dotfiles/fish/.config/fish/config.fish
+++ dotfiles/fish/.config/fish/config.fish
@@ -8,8 +8,17 @@
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 
 # pnpm
-set -gx PNPM_HOME "/Users/virajpatel/Library/pnpm"
+set -gx PNPM_HOME /Users/virajpatel/Library/pnpm
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+# mise activate
+# if type -q mise
+#     if status is-interactive
+#         mise activate fish | source
+#     else
+#         mise activate fish --shims | source
+#     end
+# end
