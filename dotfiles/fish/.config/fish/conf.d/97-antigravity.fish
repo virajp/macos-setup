1d9035c737fd028792cdef717078879af95ca89e
Viraj Patel <3125954+virajp@users.noreply.github.com>
2026-02-28 20:14:35 +0530

    feat: add ag alias for antigravity ide

---
+++ dotfiles/fish/.config/fish/conf.d/97-antigravity.fish
@@ -0,0 +1,4 @@
+# Set Antigravity alias
+if test -x "$HOME/Applications/Antigravity.app/Contents/Resources/app/bin/antigravity"
+    alias ag="$HOME/Applications/Antigravity.app/Contents/Resources/app/bin/antigravity"
+end
