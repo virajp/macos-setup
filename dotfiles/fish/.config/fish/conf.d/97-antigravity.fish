2100f3695f44828dc32aa0911a7d8b3ddb9b787b
Viraj Patel <3125954+virajp@users.noreply.github.com>
2026-02-28 20:20:26 +0530

    refactor: remove Antigravity alias from Fish configuration

--- dotfiles/fish/.config/fish/conf.d/97-antigravity.fish
+++
@@ -1,4 +0,0 @@
-# Set Antigravity alias
-if test -x "$HOME/Applications/Antigravity.app/Contents/Resources/app/bin/antigravity"
-    alias ag="$HOME/Applications/Antigravity.app/Contents/Resources/app/bin/antigravity"
-end
