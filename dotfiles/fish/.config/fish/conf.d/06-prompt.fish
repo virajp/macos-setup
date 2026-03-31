bd13a8928aa3f08753d8a15e6aff7bbec4898e81
Viraj Patel <3125954+virajp@users.noreply.github.com>
2026-03-31 19:27:38 +0530

    feat: update fish prompt configuration and add new oh-my-posh themes

--- dotfiles/fish/.config/fish/conf.d/06-prompt.fish
+++ dotfiles/fish/.config/fish/conf.d/06-prompt.fish
@@ -6,16 +6,16 @@
 # Files in conf.d/ are automatically loaded in alphabetical order.
 #
 # Shell prompt configuration:
-# - Initializes starship prompt (preferred)
-# - Alternative oh-my-posh configuration (commented out)
+# - Initializes oh-my-posh prompt (preferred)
+# - Alternative starship configuration (commented out)
 # =============================================================================
 
 # Using starship prompt
-if type -q starship
-    starship init fish | source
-end
-
-# # Using oh-my-posh prompt
-# if type -q oh-my-posh
-#   oh-my-posh init fish --config ~/.config/oh-my-posh.yaml | source
+# if type -q starship
+#     starship init fish | source
 # end
+
+# Using oh-my-posh prompt
+if type -q oh-my-posh
+    oh-my-posh init fish --config ~/.config/oh-my-posh/shell.yaml | source
+end
