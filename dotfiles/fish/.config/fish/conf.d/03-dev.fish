6b1c10a20e34fec0cc0b0aacadf593abed35d3bc
Viraj Patel <3125954+virajp@users.noreply.github.com>
2026-06-14 12:49:18 +0530

    ops: comment out Gem, Java, and Android SDK environment variable settings for clarity

--- dotfiles/fish/.config/fish/conf.d/03-dev.fish
+++ dotfiles/fish/.config/fish/conf.d/03-dev.fish
@@ -2,14 +2,14 @@
 set --global --export CHROME_EXECUTABLE "$HOME/Applications/Brave Browser.app/Contents/MacOS/Brave Browser"
 
 # Gem environment variables
-set --global --export GEM_HOME "$HOME/.gem"
-fish_add_path "$GEM_HOME/bin"
+# set --global --export GEM_HOME "$HOME/.gem"
+# fish_add_path "$GEM_HOME/bin"
 
 # Java environment variables
-set --global --export JAVA_HOME (/usr/libexec/java_home)
-set --global --export CPPFLAGS "-I$JAVA_HOME/include"
-fish_add_path "$JAVA_HOME/bin"
+# set --global --export JAVA_HOME (/usr/libexec/java_home)
+# set --global --export CPPFLAGS "-I$JAVA_HOME/include"
+# fish_add_path "$JAVA_HOME/bin"
 
 # Android SDK Path
-set --global --export ANDROID_HOME "$HOME/.android-sdk"
-fish_add_path "$ANDROID_HOME/cmdline-tools/latest/bin"
+# set --global --export ANDROID_HOME "$HOME/.android-sdk"
+# fish_add_path "$ANDROID_HOME/cmdline-tools/latest/bin"
